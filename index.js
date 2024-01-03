23356f7f6b121ec3e06a4a55f91961e6ce1c05c7
Mohammadarifcoding <minaraarifa@gmail.com>
2024-01-03 13:28:49 +0600

    Added the mongodb setup

--- index.js
+++ index.js
@@ -2,15 +2,69 @@ const express = require('express')
 const app = express()
 const port = 3000
 const cors = require('cors')
+require("dotenv").config();
+const { MongoClient, ServerApiVersion } = require('mongodb');
 
 app.use(cors())
 app.use(express.json())
 
 
 
+
+console.log(process.env.DB_USER)
+
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.pdvgnv8.mongodb.net/?retryWrites=true&w=majority`;
+
+
+
+
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+  try {
+
+
+    // Database
+    const Currency = client.db('BrightonFx').collection('Currencies')
+    
+
+
+
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+
+    app.get('/currency',async(req,res)=>{
+        const result = await Currency.find().toArray()
+        res.send(result)
+    })
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    
+  }
+}
+run().catch(console.dir);
+
+
+
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
+
+
+
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
